80796a2df25879b20acaf248dee11ccd62341d50
Park Dong Hyeok <skypia0906@gmail.com>
2023-12-30 19:09:03 +0900

    9.Lecture - rectangle

--- src/context.cpp
+++ src/context.cpp
@@ -12,11 +12,31 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
+    /*
     float vertices[] = 
     {
-        -0.5f, -0.5f, 0.0f,
+        // first triangle
+        0.5f, 0.5f, 0.0f,       // top right
+        0.5f, -0.5f, 0.0f,      // bottom right
+        -0.5f, 0.5f, 0.0f,      // top left
+        // second triangle
+        0.5f, -0.5f, 0.0f,       // bottom right
+        -0.5f, -0.5f, 0.0f,      // bottom right
+        -0.5f, 0.5f, 0.0f,       // top left
+    };
+    */
+    float vertices[] = 
+    {
+        0.5f, 0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+    };
+
+    uint32_t indices[] = 
+    {
+        0, 1, 3,        // first triangle
+        1, 2, 3,        // second triangle
     };
 
     glGenVertexArrays(1, &m_vertexArrayObject);
@@ -25,10 +45,14 @@ bool Context::Init()
     // GL Buffer를 생성, ARRAY_BUFFER 용도로 바인딩
     glGenBuffers(1, &m_vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*12, vertices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0);
+
+    glGenBuffers(1, &m_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t)*6, indices, GL_STATIC_DRAW);
 
     ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
     ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
@@ -58,5 +82,8 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(m_program->Get());
-    glDrawArrays(GL_LINE_STRIP, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    // element buffer를 사용할 경우에 사용
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
