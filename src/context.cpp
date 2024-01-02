00cb0d504f1da9cd750fd642db2d68432fa2d9ab
Park Dong Hyeok <skypia0906@gmail.com>
2024-01-02 17:46:33 +0900

    10.Lecture - multiple attribute

--- src/context.cpp
+++ src/context.cpp
@@ -12,12 +12,11 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
-    float vertices[] = 
-    {
-        0.5f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f,
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
     };
 
     uint32_t indices[] = 
@@ -31,15 +30,17 @@ bool Context::Init()
 
 
     // buffer 클래스 사용
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*12);
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*24);
 
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0);
+    // m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, sizeof(float)*3);
 
     // buffer 클래스 사용
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t)*6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
 
     if(!vertShader || !fragShader)
     {
@@ -56,11 +57,6 @@ bool Context::Init()
     }
     SPDLOG_INFO("program id: {}", m_program->Get());
 
-    // shader의 uniform에 값을 세팅
-    auto loc = glGetUniformLocation(m_program->Get(), "color");
-    m_program->Use();
-    glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
-
     // color setting for clearing
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
 
@@ -71,14 +67,9 @@ void Context::Render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    static float time = 0.0f;
-    float t = sinf(time) * 0.5f + 0.5f;
-    auto loc = glGetUniformLocation(m_program->Get(), "color");;
     m_program->Use();
-    glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    time += 0.016f;
-
+    
     // m_program->Use();
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
