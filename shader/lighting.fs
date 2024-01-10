55037991cb4e545defc124b9ef73dd3681e5ae01
Park Dong Hyeok <skypia0906@gmail.com>
2024-01-10 16:21:10 +0900

    21.Lecture - apply material

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -5,38 +5,48 @@ in vec3 position;
 in vec2 texCoord;
 out vec4 fragColor;
 
-uniform vec3 lightPos;              // 빛의 위치
-uniform vec3 lightColor;            // 광원의 색상
-uniform vec3 objectColor;           // 그릴려고 하는 object의 색상
-uniform float ambientStrength;      // light color의 강도 결정
-
-uniform float specularStrength;
-uniform float specularShininess;
 uniform vec3 viewPos;               // 바라보는 눈(카메라)의 위치
+
+struct Light
+{
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform Light light;
+
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;            // 표면이 얼마나 반짝거리는지 표현
+};
+uniform Material material;
 
 void main()
 {
     // ambient 계산
-    vec3 ambient = ambientStrength * lightColor;
+    // 재질 자체가 얼마나 빛나는가 * light의 ambient
+    vec3 ambient = material.ambient * light.ambient;
 
     // diffuse 계산
-    vec3 lightDir = normalize(lightPos - position);
+    vec3 lightDir = normalize(light.position - position);
     vec3 pixelNorm = normalize(normal);
-    vec3 diffuse = max(dot(pixelNorm, lightDir), 0.0) * lightColor;
+    // diffuse factor * material의 diffuse * light의 diffuse
+    float diff = max(dot(pixelNorm, lightDir), 0.0);
+    vec3 diffuse = diff * material.diffuse * light.diffuse;
 
     // specular 계산
     vec3 viewDir = normalize(viewPos - position);
-    // lightDir 방향으로 입사한 빛이 다시 반사되어 나오는 방향벡터를 구함
-    // -lightDir이 입사한 방향을 의미
     vec3 reflectDir = reflect(-lightDir, pixelNorm);
-    // 지수함수를 쓰는 이유는 
-    // - max 결과가 0~1 사이인데, specularShininess가 커질수록 값이 작아진다
-    // - specular light가 만드는 영역이 생기는데, 점점 작아지는 효과를 만든다
-    // - specular light의 밝기가 아닌 만드는 영역을 제어하는 목적
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), specularShininess);
-    vec3 specular = specularStrength * spec * lightColor;
+    // specular factor
+    // specular factor * material의 specular * light의 specular
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = spec * material.specular * light.specular;
 
-    // 최종 결과
-    vec3 result = (ambient + diffuse+ specular) * objectColor;
+    // 최종 Color
+    vec3 result = ambient + diffuse+ specular;
     fragColor = vec4(result, 1.0);
 }
