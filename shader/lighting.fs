68d34a3bf9d2f26b27c041c9ae24418ce167f509
Park Dong Hyeok <skypia0906@gmail.com>
2024-01-26 15:17:10 +0900

    32.Lecture - add blind_phong shading code

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -18,6 +18,7 @@ struct Light
     vec3 specular;
 };
 uniform Light light;
+uniform int blinn;
 
 struct Material
 {
@@ -54,11 +55,22 @@ void main()
         vec3 diffuse = diff * texColor * light.diffuse;
 
         vec3 specColor = texture2D(material.specular, texCoord).xyz;
-        vec3 viewDir = normalize(viewPos - position);
-        vec3 reflectDir = reflect(-lightDir, pixelNorm);
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        float spec = 0.0;
+        if(blinn == 0)
+        {
+            // phong shading
+            vec3 viewDir = normalize(viewPos - position);
+            vec3 reflectDir = reflect(-lightDir, pixelNorm);
+            spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        }
+        else
+        {
+            // blinn-phong shading
+            vec3 viewDir = normalize(viewPos - position);
+            vec3 halfDir = normalize(lightDir +; viewDir);
+            spec = pow(max(dot(halfDir, pixelNorm), 0.0), material.shininess);
+        }
         vec3 specular = spec * specColor * light.specular;
-
         result += (diffuse + specular)*intensity;
     }
 
