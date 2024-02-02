c673bc7010cc610b1ea9a11fcc2e757e2f192979
Park Dong Hyeok <skypia0906@gmail.com>
2024-02-02 17:16:30 +0900

    42.Lecture - Context class refactoring for PBR

--- src/context.h
+++ src/context.h
@@ -14,8 +14,7 @@
 
 
 CLASS_PTR(Context)
-class Context
-{
+class Context {
 public:
     static ContextUPtr Create();
     void Render();
@@ -23,112 +22,30 @@ public:
     void Reshape(int width, int height);
     void MouseMove(double x, double y);
     void MouseButton(int button, int action, double x, double y);
+
     void DrawScene(const glm::mat4& view, const glm::mat4& projection, const Program* program);
 
 private:
     Context() {}
     bool Init();
-    ProgramUPtr m_program;
-    ProgramUPtr m_simpleProgram;            // light 위치/박스를 위한 program, 단색으로 그림을 그리는 shader program
-    ProgramUPtr m_textureProgram;           // texture program
-    ProgramUPtr m_postProgram;              // post processing program
-
-    float m_gamma { 1.0f };
+  
+    ProgramUPtr m_simpleProgram;
 
     MeshUPtr m_box;
     MeshUPtr m_plane;
 
+    // screen size
+    int m_width {640};
+    int m_height {480};
 
-    // animation
-    bool m_animation { true };
-
-    // clear color
-    glm::vec4 m_clearColor { glm::vec4(0.1f, 0.2f, 0.3f, 0.0f) };
-
-    // light parameter
-    struct Light 
-    {
-        bool directional { false };
-        glm::vec3 position { glm::vec3(2.0f, 4.0f, 4.0f) };
-        glm::vec3 direction { glm::vec3(-0.5f, -1.5f, -1.0f) };
-        glm::vec2 cutoff { glm::vec2(50.0f, 5.0f) };
-        float distance { 150.0f };
-        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
-        glm::vec3 diffuse { glm::vec3(0.8f, 0.8f, 0.8f) };
-        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
-    };
-    Light m_light;
-    bool m_flashLightMode { false };
-    bool m_blinn { true };
-
-    MaterialPtr m_planeMaterial;
-    MaterialPtr m_box1Material;
-    MaterialPtr m_box2Material;
-    TexturePtr m_windowTexture;
-
-    // camera parameters
-    float m_cameraPitch { -20.0f };
-    float m_cameraYaw { 0.0f };
-    glm::vec3 m_cameraPos { glm::vec3(0.0f, 2.5f, 8.0f) };          // 원전에서 (0, 0, 3)만큼 떨어져있는
-    glm::vec3 m_cameraFront { glm::vec3(0.0f, -1.0f, 0.0f) };       // -z축을 바라보도록
-    glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };           // camera의 up vector
-
+    // camera parameter
     bool m_cameraControl { false };
     glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
-
-    // framebuffer
-    FramebufferUPtr m_framebuffer;
-
-    // cubemap
-    CubeTextureUPtr m_cubeTexture;
-    ProgramUPtr m_skyboxProgram;
-    ProgramUPtr m_envMapProgram;
-
-    // grass
-    TexturePtr m_grassTexture;
-    ProgramUPtr m_grassProgram;
-    std::vector<glm::vec3> m_grassPos;
-
-    // for instancing
-    BufferUPtr m_grassPosBuffer;
-    VertexLayoutUPtr m_grassInstance;
-
-    // shadow map
-    ShadowMapUPtr m_shadowMap;
-    ProgramUPtr m_lightingShadowProgram;
-
-    // normal map
-    TextureUPtr m_brickDiffuseTexture;
-    TextureUPtr m_brickNormalTexture;
-    ProgramUPtr m_normalProgram;
-
-    // deffered shading
-    FramebufferUPtr m_deferGeoFramebuffer;
-    ProgramUPtr m_deferGeoProgram;
-    ProgramUPtr m_deferLightProgram;
-
-    struct DeferLight 
-    {
-        glm::vec3 position;
-        glm::vec3 color;
-    };
-    std::vector<DeferLight> m_deferLights;
-
-    // ssao
-    FramebufferUPtr m_ssaoFramebuffer;
-    ProgramUPtr m_ssaoProgram;
-    ModelUPtr m_model;                  // 가방 모델, for test rendering
-    TextureUPtr m_ssaoNoiseTexture;
-    std::vector<glm::vec3> m_ssaoSamples;
-    float m_ssaoRadius { 1.0f };
-    
-    // blur for ssao
-    ProgramUPtr m_blurProgram;
-    FramebufferUPtr m_ssaoBlurFramebuffer;
-    bool m_useSsao { true };
-
-    int m_width { WINDOW_WIDTH };
-    int m_height { WINDOW_HEIGHT };
+    float m_cameraPitch { 0.0f };
+    float m_cameraYaw { 0.0f };
+    glm::vec3 m_cameraFront { glm::vec3(0.0f, -1.0f, 0.0f) };
+    glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 8.0f) };
+    glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
 };
 
 #endif
